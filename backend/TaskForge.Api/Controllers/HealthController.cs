9839ad268f2a14a3e34285613c1b938c8163d7fc
Sujendran Reddy <ST10457761@vcconnect.edu.za>
2026-09-02 17:16:29 +0200

    Add project files.

---
+++ backend/TaskForge.Api/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskForge.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok(new
+            {
+                status = "TaskForge API is running"
+            });
+        }
+    }
+}
